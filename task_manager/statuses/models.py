117646da313ca9cd359d62d73b2aadb2b7bae3ba
Sergey Radyukhin <22dollars@mail.ru>
2023-01-16 18:32:28 +0300

    fix statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from task_manager.users.models import CustomUser
-from task_manager.statuses.models import Status
-from task_manager.labels.models import Label
 from django.utils.translation import gettext_lazy as _
+
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+from task_manager.users.models import CustomUser
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=50, unique=True, verbose_name=_('Name'))
+    description = models.TextField(null=True, blank=True,
+                                   verbose_name=_('Description'))
     owner = models.ForeignKey(
         CustomUser,
         related_name='owner',
@@ -22,7 +24,7 @@ class Task(models.Model):
         verbose_name=_('Executor'),
     )
     status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True,
-                               verbose_name=_('Status'),)
+                               verbose_name=_('Status'))
     labels = models.ManyToManyField(
         Label,
         verbose_name=_('Labels'),
