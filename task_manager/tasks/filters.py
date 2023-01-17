2e83a2307312d713e9e565c57917c421cffdd564
Sergey Radyukhin <22dollars@mail.ru>
2023-01-17 18:14:40 +0300

    label labels who cares

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -2,7 +2,7 @@ import django_filters
 from django import forms
 from task_manager.tasks.models import Task
 from task_manager.labels.models import Label
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 
 
 class TaskFilter(django_filters.FilterSet):
@@ -10,13 +10,14 @@ class TaskFilter(django_filters.FilterSet):
         return queryset.filter(owner=self.request.user)
 
     labels = django_filters.ModelChoiceFilter(
-        queryset=Label.objects.all()
+        queryset=Label.objects.all(),
+        label=_('Label'),
     )
 
     self_tasks = django_filters.BooleanFilter(
         widget=forms.CheckboxInput(),
         method='get_self_tasks',
-        label=gettext('My tasks only'),
+        label=_('My tasks only'),
     )
 
     class Meta:
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -29,6 +29,6 @@ class Task(models.Model):
     labels = models.ManyToManyField(
         Label,
         blank=True,
-        verbose_name=_('Label'),
+        verbose_name=_('Labels'),
     )
     created_at = models.DateTimeField(auto_now_add=True)
